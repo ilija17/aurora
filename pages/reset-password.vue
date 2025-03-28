23090996dbaa708db92dfb70e290066b643109b8
Ilija <ilijabosnjak@tutanota.com>
2025-03-28 11:46:30 +0100

    i love minor (changes)

---
+++ pages/reset-password.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h2>Set a new password</h2>
+    <input v-model="password" type="password" placeholder="New password" />
+    <button @click="updatePassword">Update password</button>
+    <p v-if="message">{{ message }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+const supabase = useSupabaseClient()
+const password = ref('')
+const message = ref('')
+const user = useSupabaseUser()
+
+const updatePassword = async () => {
+  if (!password.value) {
+    message.value = 'Please enter a new password'
+    return
+  }
+
+  const { data, error } = await supabase.auth.updateUser({
+    password: password.value,
+  })
+
+  if (error) {
+    message.value = error.message
+  } else {
+    message.value = 'Password updated! You can now log in.'
+    // Optional: Redirect to login
+    setTimeout(() => navigateTo('/login'), 2000)
+  }
+}
+</script>
