e3c617a9bc65de4493ec47f10adb3b0211c57b0e
Ilija <ilijabosnjak@tutanota.com>
2025-05-26 14:38:58 +0200

    ship it

--- server/utils/spotify.ts
+++ server/utils/spotify.ts
@@ -1,8 +1,7 @@
-import type { H3Event } from 'h3'
 import { getCookie } from 'h3'
+import type { H3Event } from 'h3'
 
-export function hasValidSpotifyToken (event: H3Event): boolean {
-  const access  = getCookie(event, 'spotify_access')
-  const expires = Number(getCookie(event, 'spotify_expires') || 0)
-  return Boolean(access && Date.now() < expires)
+export function hasValidSpotifyToken (event: H3Event) {
+  const token = getCookie(event, 'spotify_access_token')
+  return Boolean(token)
 }
